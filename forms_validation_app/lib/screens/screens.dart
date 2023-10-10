e75e4ffe60b6306f38c7a3dd5a4fd67bd63eb18c
pgarcialcrs <pgarcia@lacadenaretail.com>
2023-10-10 09:45:45 -0500

    forms validation app deleted

--- forms_validation_app/lib/screens/screens.dart
+++
@@ -1,2 +0,0 @@
-export 'package:forms_validation_app/screens/home_screen.dart';
-export 'package:forms_validation_app/screens/login_screen.dart';
--- forms_validation_app/lib/widgets/widgets.dart
+++
@@ -1,2 +0,0 @@
-export 'package:forms_validation_app/widgets/auth_background.dart';
-export 'package:forms_validation_app/widgets/card_container.dart';
